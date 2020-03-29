ca8fafb8676c330323d208c84f4a9b30a36a0a64
Jean Carlos Nunez <jc@tiempoexacto.com>
2020-03-29 11:10:30 -0500

    initial commir

---
+++ app/src/main/java/com/course/plazit/ui/fragments/UbicationsFragment.kt
@@ -0,0 +1,24 @@
+package com.course.plazit.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.course.plazit.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class UbicationsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ubications, container, false)
+    }
+
+}
